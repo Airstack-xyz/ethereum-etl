2f0298d8ce97353f3ac38ba67ece2c0ea965335f
Manjeet Thadani <manjeet.thadani@gmail.com>
2024-04-08 15:27:24 +0530

    refactor code

--- ethereumetl/cli/stream.py
+++ ethereumetl/cli/stream.py
@@ -75,8 +75,8 @@ def stream(last_synced_block_file, lag, output, start_block, end_block, entity_t
     if os.environ['KAFKA_BROKER_URI'] == None:
         raise ValueError('KAFKA_BROKER_URI env is missing')
     
-    if os.environ['SYNC_MODE'] == None:
-        raise ValueError('SYNC_MODE env is missing')
+    if os.environ['SYNC_MODE'] == None or os.environ['SYNC_MODE'] not in constants.VALID_SYNC_MODES:
+        raise ValueError('SYNC_MODE env is missing or incorrect')
     
     if os.environ['METRICS_PORT'] == None:
         raise ValueError('METRICS_PORT env is missing')
